128a781fd39e7044f0820539d303362a3b31843b
Patrick <patrick.motsebo@tech901.org>
2020-04-02 10:24:36 -0500

    Updates

---
+++ precache-manifest.b39c35beb27ebbe7e3c4a2ef4cd72c71.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6097a6ecbae405b17d22696b88f51ebc",
+    "url": "/Yelp_App1/index.html"
+  },
+  {
+    "revision": "827ff5ed7fd840479a61",
+    "url": "/Yelp_App1/static/css/main.73e27983.chunk.css"
+  },
+  {
+    "revision": "6dca9c975252f1109216",
+    "url": "/Yelp_App1/static/js/2.d4eaff96.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Yelp_App1/static/js/2.d4eaff96.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "827ff5ed7fd840479a61",
+    "url": "/Yelp_App1/static/js/main.8031163c.chunk.js"
+  },
+  {
+    "revision": "92fb9b55247acc1fc117",
+    "url": "/Yelp_App1/static/js/runtime-main.b6c0f758.js"
+  },
+  {
+    "revision": "a677bab3561bd4a37b5f4253690e765b",
+    "url": "/Yelp_App1/static/media/foodbackground.a677bab3.jpg"
+  }
+]);
