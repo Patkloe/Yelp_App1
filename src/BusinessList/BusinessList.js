3191d0f17e7b5d6591804d7189bc9c630968f9d3
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-04-15 16:24:08 -0500

    Add files via upload

--- src/BusinessList/BusinessList.js
+++ src/BusinessList/BusinessList.js
@@ -1,25 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
 import './BusinessList.css';
-import Business from './Business/Business.js';
+import Business from '../Business/Business.js';
+
+
 class BusinessList extends React.Component {
-  render() {
-    return{(
-          <div className="BusinessList">
-          this.props.businesses.map(business => {
-          <Business business={business} />
-          })
-         </div>
-    )};
-  }
-}
+    constructor(props){
+        super(props);
+    }
+    
+    render() {
+            //let val = 0;
+            let restrauntInfo = this.props.businesses.map((business) => {
+           // let val = restrauntInfo.length;
+            //if(val == 0)
+             // {
+                //  return( alert('Unknown Business'));
+             // }
+     //render() {
+            let name = business['name'];
+            let image = business['imageSrc']
+            let address = business['address'];
+            let city = business['city'];
+            let state = business['state'];
+            let zip = business['zipCode'];
+            let rating = business['rating'];
+            let review = business['reviewCount'];
+            let url = business['url'];
+            return <Business 
+                key={business.id}
+                image={image}
+                name={name} 
+                address={address} 
+                city={city} 
+                state={state} 
+                zipCode={zip} 
+                rating={rating} 
+                reviewCount={review}
+                url={url}
+                />
+        })
+         let res = restrauntInfo.length;
+         console.log(restrauntInfo);
+         console.log(res);
+        return(
+            <div className="BusinessList">
+                {restrauntInfo}
+            </div>
+        );
+       // console.log(5);
+    };
+};
+//console.log(" test a voir" + {restrauntInfo});
 export default BusinessList;
-
-
-
-
-
-
-
-
-
-
